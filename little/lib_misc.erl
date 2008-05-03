06da4ac3c1ea38a2529834b84c87b4447b8d62b3
Luke Amdor <luke@absoluterubbish.net>
2008-05-03 12:47:09 -0500

    Added odds_and_evens (with and without acuumulators)

--- little/lib_misc.erl
+++ little/lib_misc.erl
@@ -26,3 +26,20 @@ pythagorean_triplets(N) ->
 % Find all permutations of a word
 permutations([]) -> [[]];
 permutations(L) -> [ [H|T] || H <- L, T <- permutations(L -- [H]) ].
+
+% Separate list into odds and evens
+odds_and_evens(L) ->
+  Odds = [X || X <- L, X rem 2 =:= 1],
+  Evens = [X || X <- L, X rem 2 =:= 0],
+  {Odds, Evens}.
+
+odds_and_evens_acc(L) ->
+  odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H|T], Odds, Evens) ->
+  case (H rem 2) of
+    1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+    0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+  end;
+odds_and_evens_acc([], Odds, Evens) ->
+  {lists:reverse(Odds), lists:reverse(Evens)}.
