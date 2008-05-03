7ac1acace530bc40794225c11d747437dd7d75fe
Luke Amdor <luke@absoluterubbish.net>
2008-05-03 12:46:39 -0500

    Added filter function

--- little/mylists.erl
+++ little/mylists.erl
@@ -1,8 +1,16 @@
 -module (mylists).
--export ([sum/1, map/2]).
+-export ([sum/1, map/2, filter/2]).
 
 sum([H|T]) -> H + sum(T);
 sum([]) -> 0.
 
 map(F, [H|T]) -> [F(H) | map(F, T)];
 map(_, []) -> [].
+
+filter(P, [H|T]) ->
+  case P(H) of
+    true -> [H | filter(P, T)];
+    false -> filter(P, T)
+  end;
+filter(_, []) -> 
+  [].
